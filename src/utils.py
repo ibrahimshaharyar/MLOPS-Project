6991caecfc14fbf6faacca5a0ef83dd41202ce7c
ibrahimshaharyar <ibrahimshaharyar41@gmail.com>
2025-11-26 06:15:47 +0500

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
+import pickle
 import dill
 
 from sklearn.metrics import r2_score
@@ -54,5 +55,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
